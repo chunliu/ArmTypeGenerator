1cf5b3493a13f0fd62b41f5113a77df6886c86c7
Chun Liu <chunliu@microsoft.com>
2022-04-14 14:16:53 +0800

    generate classes for root schema

--- src/ArmPocoGenerator/Program.cs
+++ src/ArmPocoGenerator/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Json.Schema;
+using Microsoft.Json.Schema.ToDotNet;
+using Microsoft.Json.Schema.ToDotNet.Hints;
+
+var hintText = File.ReadAllText("CodeGenHints.json");
+
+var dmgSettings = new DataModelGeneratorSettings
+{
+    OutputDirectory = @"C:\Users\chunliu.FAREAST\experiment\ArmResources\ArmResources",
+    RootClassName = "DeploymentTemplate",
+    NamespaceName = "AzureDesignStudio.Azure",
+    ForceOverwrite = true,
+    HintDictionary = new HintDictionary(hintText),
+    ExcludedDefinitionNames = new List<string> { "proxyResourceBase", "resourceBaseExternal", "resourceLocations", "resourceKind", "resourcesWithSymbolicName" }
+};
+
+var schema = await SchemaReader.ReadSchema(new Uri("https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json"), "deploymentTemplate.json");
+
+var generator = new DataModelGenerator(dmgSettings);
+var code = generator.Generate(schema);
+
+Console.WriteLine(code);
+Console.ReadLine();
