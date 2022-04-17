37800225f3e833fafd8d343034eb6adcb4611578
Chun Liu <chunliu@microsoft.com>
2022-04-17 16:49:06 +0800

    Update code hint

--- src/ArmPocoGenerator/Program.cs
+++ src/ArmPocoGenerator/Program.cs
@@ -6,15 +6,26 @@ var hintText = File.ReadAllText("CodeGenHints.json");
 
 var dmgSettings = new DataModelGeneratorSettings
 {
-    OutputDirectory = @"C:\Users\chunliu.FAREAST\experiment\ArmResources\ArmResources",
+    OutputDirectory = @"C:\Users\chunliu.FAREAST\experiments\ArmResources\ArmResources",
     RootClassName = "DeploymentTemplate",
     NamespaceName = "AzureDesignStudio.Azure",
     ForceOverwrite = true,
     HintDictionary = new HintDictionary(hintText),
-    ExcludedDefinitionNames = new List<string> { "proxyResourceBase", "resourceBaseExternal", "resourceLocations", "resourceKind", "resourcesWithSymbolicName" }
+    ExcludedDefinitionNames = new List<string> 
+    { 
+        "proxyResourceBase", 
+        "resourceBaseExternal", 
+        "resourceLocations", 
+        "resourceKind", 
+        "resourcesWithSymbolicName", 
+        "parameterTypes",
+        "resource"
+    }
 };
 
-var schema = await SchemaReader.ReadSchema(new Uri("https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json"), "deploymentTemplate.json");
+var schema = await SchemaReader.ReadSchema(
+    new Uri("https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json"), 
+    "deploymentTemplate.json");
 
 var generator = new DataModelGenerator(dmgSettings);
 var code = generator.Generate(schema);
