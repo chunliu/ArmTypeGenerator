c65bcb6ca276a9840625c8c9ced697e3b98bbcf6
Chun Liu <chunliu@microsoft.com>
2022-04-17 18:49:56 +0800

    read settings from json

---
+++ src/ArmPocoGenerator/HintDictionaryConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Json.Schema.ToDotNet.Hints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ArmPocoGenerator
+{
+    public class HintDictionaryConverter : JsonConverter<HintDictionary>
+    {
+        public override HintDictionary? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            using JsonDocument doc = JsonDocument.ParseValue(ref reader);
+            var hintText = doc.RootElement.ToString();
+            return new HintDictionary(hintText);
+        }
+
+        public override void Write(Utf8JsonWriter writer, HintDictionary value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/ArmPocoGenerator/Program.cs
+++ src/ArmPocoGenerator/Program.cs
@@ -1,33 +1,23 @@
-﻿using Microsoft.Json.Schema;
+﻿using ArmPocoGenerator;
+using Microsoft.Json.Schema;
 using Microsoft.Json.Schema.ToDotNet;
-using Microsoft.Json.Schema.ToDotNet.Hints;
+using System.Text.Json;
 
-var hintText = File.ReadAllText("CodeGenHints.json");
-
-var dmgSettings = new DataModelGeneratorSettings
-{
-    OutputDirectory = @"C:\Users\chunliu.FAREAST\experiments\ArmResources\ArmResources",
-    RootClassName = "DeploymentTemplate",
-    NamespaceName = "AzureDesignStudio.Azure",
-    ForceOverwrite = true,
-    HintDictionary = new HintDictionary(hintText),
-    ExcludedDefinitionNames = new List<string> 
-    { 
-        "proxyResourceBase", 
-        "resourceBaseExternal", 
-        "resourceLocations", 
-        "resourceKind", 
-        "resourcesWithSymbolicName", 
-        "parameterTypes",
-        "resource"
+var config = File.ReadAllText("DeploymentTemplateConfig.json");
+var deploymentTemplateSettings = JsonSerializer.Deserialize<DataModelGeneratorSettings>(
+    config, 
+    new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        Converters = { new HintDictionaryConverter() }
     }
-};
+);
 
 var schema = await SchemaReader.ReadSchema(
     new Uri("https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json"), 
     "deploymentTemplate.json");
 
-var generator = new DataModelGenerator(dmgSettings);
+var generator = new DataModelGenerator(deploymentTemplateSettings);
 var code = generator.Generate(schema);
 
 Console.WriteLine(code);
