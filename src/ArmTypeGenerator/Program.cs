770a65c9cab4d791f9428e935a2effd4322b602b
Chun Liu <chunliu@microsoft.com>
2022-10-21 10:21:14 +0800

    recreate the project structure

--- src/ArmTypeGenerator/Program.cs
+++ src/ArmTypeGenerator/Program.cs
@@ -1,14 +1,2 @@
-﻿using ArmTypeGenerator;
-using Spectre.Console;
-using System.Reflection;
-
-Helper.LoadAppConfiguration();
-
-AnsiConsole.Write(
-    new FigletText("ARM Type Generator")
-    .Color(Color.Cyan1));
-AnsiConsole.MarkupLine($"ArmTypeGenerator, " +
-    $"v{Assembly.GetExecutingAssembly()!.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion}");
-AnsiConsole.WriteLine();
-
-await Helper.MainFlow();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
